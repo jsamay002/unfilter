da5a9c309a8c4f506bb5f32c31718b13846205d8
jsamay002 <jsamay002@gmail.com>
2026-02-15 21:27:28 -0600

    chore: project boots with routes scaffolded

--- .next/dev/types/validator.ts
+++ .next/dev/types/validator.ts
@@ -99,10 +99,10 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
   type __Unused = __Check
 }
 
-// Validate ../../../src/app/(marketing)/page.tsx
+// Validate ../../../src/app/(marketing)/landing/page.tsx
 {
-  type __IsExpected<Specific extends AppPageConfig<"/">> = Specific
-  const handler = {} as typeof import("../../../src/app/(marketing)/page.js")
+  type __IsExpected<Specific extends AppPageConfig<"/landing">> = Specific
+  const handler = {} as typeof import("../../../src/app/(marketing)/landing/page.js")
   type __Check = __IsExpected<typeof handler>
   // @ts-ignore
   type __Unused = __Check
