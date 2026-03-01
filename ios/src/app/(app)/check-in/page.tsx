1c1a429d22a76bf7e9bb077fe7413631913aecdf
jsamay002 <jsamay002@gmail.com>
2026-03-01 16:44:41 -0600

    Update iOS SwiftUI port - v10 rebuild

--- ios/src/app/(app)/check-in/page.tsx
+++
@@ -1,5 +0,0 @@
-import CheckInWizard from "@/features/checkin/CheckInWizard";
-
-export default function CheckInPage() {
-  return <CheckInWizard />;
-}
--- ios/src/features/checkin/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as CheckInWizard } from "./CheckInWizard";
-export { QualityGateCard } from "./components/quality-gate/QualityGateCard";
-export { SymptomForm } from "./components/questions/SymptomForm";
-export { ResultsCard } from "./components/results/ResultsCard";
-export { AnalyzingSpinner } from "./components/AnalyzingSpinner";
-export * from "./types";
